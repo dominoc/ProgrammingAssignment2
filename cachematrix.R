9f3d9820b5ba880848400cbbcbac5b4f1e20409c
Dominic Chin <dominicchin@yahoo.com>
2015-06-09 15:57:25 +0800

    added code for solving the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,29 @@
 ## functions do
 
 ## Write a short comment describing this function
+## 
+## m <- matrix ( c(2, 3,   2, 2), nrow = 2, ncol = 2, byrow = TRUE)
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inv <- NULL
+  
+  set <- function (y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function () x
+  
+  setinverse <- function (inverse) inv <<- inverse
+  
+  getinverse <- function () inv
+  
+  list ( set = set, 
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+  
 }
 
 
@@ -12,4 +32,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return (inv)
+  }
+  
+  the_matrix <- x$get()
+  
+  inv <- solve(the_matrix, ...)
+  x$setinverse(inv)
+  inv
 }
